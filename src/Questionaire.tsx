0e0300815f508ed399de552000f5af6d33df1ace
Marina Olivella <marina@openearth.org>
2025-03-25 14:16:28 -0300

    centered survey

--- src/Questionaire.tsx
+++ src/Questionaire.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {Model} from "survey-core";
 import {Survey} from "survey-react-ui";
 import "survey-core/survey-core.min.css";
@@ -6,6 +6,7 @@ import "./index.css";
 import {json} from "./json.js";
 import {Question} from "./types.ts";
 import i18n from "i18next";
+import {Box} from "@chakra-ui/react";
 
 interface QuestionnaireProps {
     setAnswers: (value: (((prevState: (Question[] | undefined)) => (Question[] | undefined)) | Question[] | undefined)) => void,
@@ -17,6 +18,22 @@ function SurveyComponent({setAnswers, answers}: QuestionnaireProps) {
 console.log('Selected language:', selectedLanguage);
     const survey = new Model(json);
     survey.locale = selectedLanguage;
+    survey.css = {
+        root: "survey-custom-centered",
+        container: "survey-container-centered"
+    };
+
+    // Apply styling after component mounts
+    useEffect(() => {
+        // Find the main survey element and apply styles
+        const surveyMain = document.querySelector('.sv_main');
+        if (surveyMain) {
+            surveyMain.style.margin = '0 auto';
+            surveyMain.style.maxWidth = '800px';
+            surveyMain.style.width = '100%';
+        }
+    }, []);
+
     survey.onComplete.add((sender, options) => {
         console.log(JSON.stringify(sender.data, null, 3));
         console.log("options", JSON.stringify(options, null, 2)) // TODO NINA
